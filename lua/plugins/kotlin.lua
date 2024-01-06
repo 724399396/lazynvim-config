434a33437e684942d6ee69044836a0c99af1cf7f
Wei Li <liwei72439936@gmail.com>
2024-01-06 20:51:50 +0800

    kotlin config

--- lua/plugins/kotlin.lua
+++ lua/plugins/kotlin.lua
@@ -1,9 +1,30 @@
 return {
   {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "kotlin" })
+      end
+    end,
+  },
+  {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
         kotlin_language_server = {},
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    dependencies = {
+      {
+        "williamboman/mason.nvim",
+        opts = function(_, opts)
+          opts.ensure_installed = opts.ensure_installed or {}
+          vim.list_extend(opts.ensure_installed, { "kotlin-debug-adapter" })
+        end,
       },
     },
   },
