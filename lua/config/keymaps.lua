788eadc6d5d7102fe443cfd13c9465a8ff09a39a
Wei Li <liwei72439936@gmail.com>
2024-01-12 21:29:50 +0800

    remove windows navigator in window

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 --
+vim.keymap.del("t", "<C-h>")
+vim.keymap.del("t", "<C-j>")
+vim.keymap.del("t", "<C-k>")
+vim.keymap.del("t", "<C-l>")
