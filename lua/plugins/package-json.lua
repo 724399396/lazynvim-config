8f320e3fad151b953e28d9f2e9155da741c8cb0d
Wei Li <liwei72439936@gmail.com>
2024-09-14 10:07:02 +0800

    add package.json plugin

---
+++ lua/plugins/package-json.lua
@@ -0,0 +1,29 @@
+return {
+  "vuki656/package-info.nvim",
+  dependencies = { "MunifTanjim/nui.nvim" },
+  ft = "json",
+  opts = {},
+  keys = {
+    {
+      "<leader>cps",
+      function()
+        require("package-info").show()
+      end,
+      desc = "Show package information",
+    },
+    {
+      "<leader>cpd",
+      function()
+        require("package-info").delete()
+      end,
+      desc = "Delete package",
+    },
+    {
+      "<leader>cpc",
+      function()
+        require("package-info").change_version()
+      end,
+      desc = "Change package version",
+    },
+  },
+}
